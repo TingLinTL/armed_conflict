937c917f5609ab10100183af24baf9d179a396f8
TingLinTL <Tarynlin0101@gmail.com>
2024-09-16 11:27:57 -0400

    Initial commit

---
+++ script.R
@@ -0,0 +1,7 @@
+library(here)
+here()
+rawdata<- read.csv(here("original", "maternalmortality.csv"), header = TRUE)
+library(tidyverse)
+newdata <- rawdata %>% select(Country.Name, X2000:X2019)
+newdata1 <- newdata %>% pivot_longer(cols=X2000:X2019, names_to = "Year", names_prefix = "X", values_to = "MatMor")
+newdata2 <- newdata1 %>% mutate(Year = as.numeric(Year)) 
