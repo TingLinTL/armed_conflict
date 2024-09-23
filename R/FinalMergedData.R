6a6d3143d64107d0e0a3298fc85b3105fb5ec821
TingLinTL <Tarynlin0101@gmail.com>
2024-09-23 13:42:12 -0400

    one more code for saving as CSV

--- R/FinalMergedData.R
+++ R/FinalMergedData.R
@@ -7,6 +7,8 @@ covariates <- read.csv(here("original", "covariates.csv"), header = TRUE)
 allfinal <- list(final_allmortality, newconflict2, covariates,newdisdata)
 merged_all_con <- reduce(allfinal, full_join, by = c("year","ISO"))
 
+#save as CSV
+write.csv(merged_all_con, "merged_all_con.csv", row.names = FALSE)
 
 
                          
