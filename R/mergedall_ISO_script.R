d145c48c23f19c1a47654420d9a363c17a144532
TingLinTL <Tarynlin0101@gmail.com>
2024-09-23 10:56:27 -0400

    Merged all mortality data, added ISO, removed Country Name

---
+++ R/mergedall_ISO_script.R
@@ -0,0 +1,41 @@
+# clean data
+cleandata <- function(file_path){
+  x <- read.csv(file_path, header = TRUE)
+  x %>% 
+    select(Country.Name, X2000:X2019) %>% 
+    pivot_longer(cols = X2000:X2019, names_to = "Year", names_prefix = "X", values_to = "MatMor") %>% 
+    mutate(Year = as.numeric(Year)) 
+}
+
+# four "new" data sets
+newdata_maternalmortality <- cleandata(here("original", "maternalmortality.csv"))
+newdata_infantmortality <- cleandata(here("original", "infantmortality.csv"))
+newdata_neonatalmortality <- cleandata(here("original", "neonatalmortality.csv"))
+newdata_under5mortality <- cleandata(here("original", "under5mortality.csv"))
+
+# merge four data sets
+allmortality  <- list(newdata_maternalmortality, newdata_infantmortality, newdata_neonatalmortality, newdata_under5mortality)
+merged_allmortality <- reduce(allmortality, full_join, by = c("Country.Name", "Year"))
+
+rename_merged_allmortality <- merged_allmortality %>% 
+  rename(
+    CountryName = Country.Name,
+    Year = Year,
+    MaternalMortalityRate = MatMor.x,
+    InfantMortalityRate = MatMor.y,
+    NeonatalMortalityRate = MatMor.x.x,
+    Under5MortalityRate = MatMor.y.y
+  )
+
+#add ISO-3c countrycode
+library(countrycode)
+rename_merged_allmortality$ISO <- countrycode(rename_merged_allmortality$CountryName,
+                                              origin = "country.name",
+                                              destination = "iso3c")
+
+
+# Remove the CountryName variable
+final_allmortality <- rename_merged_allmortality %>%
+  select(-CountryName)
+
+head(final_allmortality)
--- R/script.R
+++ R/script.R
@@ -1,7 +1,9 @@
 library(here)
+library(tidyverse)
 here()
 rawdata<- read.csv(here("original", "maternalmortality.csv"), header = TRUE)
-library(tidyverse)
+
 newdata <- rawdata %>% select(Country.Name, X2000:X2019)
 newdata1 <- newdata %>% pivot_longer(cols=X2000:X2019, names_to = "Year", names_prefix = "X", values_to = "MatMor")
 newdata2 <- newdata1 %>% mutate(Year = as.numeric(Year)) 
+
