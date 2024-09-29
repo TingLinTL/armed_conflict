dba5a73ab4458777f784336dea31f86e75240348
TingLinTL <Tarynlin0101@gmail.com>
2024-09-29 16:54:44 -0400

    reorder the final merged table

--- R/FinalMergedData.R
+++ R/FinalMergedData.R
@@ -6,6 +6,11 @@ source("R/disaster_script.R")
 covariates <- read.csv(here("original", "covariates.csv"), header = TRUE)
 allfinal <- list(final_allmortality, newconflict2, covariates,newdisdata)
 merged_all_con <- reduce(allfinal, full_join, by = c("year","ISO"))
+names(merged_all_con)
+merged_all_con <- merged_all_con %>% select(country_name, ISO, region, year, gdp1000, OECD, OECD2023, popdens, urban, agedep, male_edu, 
+                                            temp, rainfall1000, totaldeath, armconf1, MaternalMortalityRate, InfantMortalityRate, 
+                                            NeonatalMortalityRate, Under5MortalityRate, drought, earthquake)
+
 
 #save as CSV
 write.csv(merged_all_con, "merged_all_con.csv", row.names = FALSE)
