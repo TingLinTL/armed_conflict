96692d3ce7d81a65499b276fce0d7520e70163a6
TingLinTL <Tarynlin0101@gmail.com>
2024-09-29 16:33:13 -0400

    :(

--- R/lagged_conflict.R
+++ R/lagged_conflict.R
@@ -4,5 +4,5 @@ here()
 rawconflict <- read.csv(here("original", "conflictdata.csv"), header = TRUE)
 
 newconflict1 <- rawconflict %>% group_by(year, ISO) %>% summarize(totaldeath= sum(best))
-newconflict2 <- newconflict1 %>% mutate(armconf1 = ifelse(conflict >= 25,1,0))
+newconflict2 <- newconflict1 %>% mutate(armconf1 = ifelse(totaldeath >= 25,1,0))
 newconflict2$year <- newconflict2$year + 1
