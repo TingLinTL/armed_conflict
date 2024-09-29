07ee14c0f215ec0094ada7ead8da2d6ae070a591
TingLinTL <Tarynlin0101@gmail.com>
2024-09-29 16:29:05 -0400

    renamed variables. changed "conflict" as "totaldeath" & "indicator" as "armconf1"

--- R/lagged_conflict.R
+++ R/lagged_conflict.R
@@ -3,6 +3,6 @@ library(tidyverse)
 here()
 rawconflict <- read.csv(here("original", "conflictdata.csv"), header = TRUE)
 
-newconflict1 <- rawconflict %>% group_by(year, ISO) %>% summarize(conflict= sum(best))
-newconflict2 <- newconflict1 %>% mutate(indicator = ifelse(conflict >= 25,1,0))
+newconflict1 <- rawconflict %>% group_by(year, ISO) %>% summarize(totaldeath= sum(best))
+newconflict2 <- newconflict1 %>% mutate(armconf1 = ifelse(conflict >= 25,1,0))
 newconflict2$year <- newconflict2$year + 1
