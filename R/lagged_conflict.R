1fa1a74ea941a1db022a6fac9d75f8d1993e7e65
TingLinTL <Tarynlin0101@gmail.com>
2024-09-30 13:06:31 -0400

    revised

--- R/lagged_conflict.R
+++ R/lagged_conflict.R
@@ -7,3 +7,4 @@ newconflict1 <- rawconflict %>% group_by(year, ISO) %>% summarize(totaldeath= su
 newconflict1$totaldeath[is.na(newconflict1$totaldeath)]=0
 newconflict2 <- newconflict1 %>% mutate(armconf1 = ifelse(totaldeath >= 25,1,0))
 newconflict2$year <- newconflict2$year + 1
+
