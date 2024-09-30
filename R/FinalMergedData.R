21830d9a2b7fa27d8ed9f4d69b3ed1464a19a211
TingLinTL <Tarynlin0101@gmail.com>
2024-09-30 11:17:51 -0400

    revised

--- R/FinalMergedData.R
+++ R/FinalMergedData.R
@@ -4,16 +4,21 @@ source("R/mergedall_ISO_script.R")
 source("R/lagged_conflict.R")
 source("R/disaster_script.R")
 covariates <- read.csv(here("original", "covariates.csv"), header = TRUE)
-allfinal <- list(final_allmortality, newconflict2, covariates,newdisdata)
-merged_all_con <- reduce(allfinal, full_join, by = c("year","ISO"))
-names(merged_all_con)
-merged_all_con <- merged_all_con %>% select(country_name, ISO, region, year, gdp1000, OECD, OECD2023, popdens, urban, agedep, male_edu, 
+sub_allfinal <- list(final_allmortality, newconflict2, newdisdata)
+sub_merged_all_con <- reduce(sub_allfinal, full_join, by = c("year","ISO"))
+allfinal <- left_join(covariates, sub_merged_all_con, by = c("year","ISO"))
+
+
+names(allfinal)
+allfinal <- allfinal %>% select(country_name, ISO, region, year, gdp1000, OECD, OECD2023, popdens, urban, agedep, male_edu, 
                                             temp, rainfall1000, totaldeath, armconf1, MaternalMortalityRate, InfantMortalityRate, 
                                             NeonatalMortalityRate, Under5MortalityRate, drought, earthquake)
+allfinal$earthquake[is.na(allfinal$earthquake)]=0
+allfinal$drought[is.na(allfinal$drought)]=0
 
 
-#save as CSV
-write.csv(merged_all_con, "merged_all_con.csv", row.names = FALSE)
+# save as CSV
+# write.csv(allfinal, "merged_all_con.csv", row.names = FALSE)
 
 
                          
--- R/lagged_conflict.R
+++ R/lagged_conflict.R
@@ -4,5 +4,6 @@ here()
 rawconflict <- read.csv(here("original", "conflictdata.csv"), header = TRUE)
 
 newconflict1 <- rawconflict %>% group_by(year, ISO) %>% summarize(totaldeath= sum(best))
+newconflict1$totaldeath[is.na(newconflict1$totaldeath)]=0
 newconflict2 <- newconflict1 %>% mutate(armconf1 = ifelse(totaldeath >= 25,1,0))
 newconflict2$year <- newconflict2$year + 1
