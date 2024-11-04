de28eacc52202699fbd7471946c63e9f8b4c3930
Ting Lin <Tarynlin0101@gmail.com>
2024-11-04 12:08:14 -0500

    revised

--- R/FinalDataSet.R
+++ R/FinalDataSet.R
@@ -5,7 +5,6 @@ data$armconf1 <- ifelse(is.na(data$armconf1), 0, data$armconf1)
 data$totaldeath <- ifelse(is.na(data$totaldeath), 0, data$totaldeath)
 data$popdens = data$popdens /100
 data$loggdp1000 <- log(data$gdp1000)
-data$popdens = data$popdens /100
 
 # save as CSV
 write.csv(data, "FinalDataset.csv", row.names = FALSE)
